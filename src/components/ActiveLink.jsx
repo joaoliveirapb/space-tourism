8c30f273d283c82f2e9a24e0f197eed70066fadf
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-05-22 15:43:26 -0300

    created active link functionality in the header

---
+++ src/components/ActiveLink.jsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export function ActiveLink({ content, index, href }) {
+  const pathname = usePathname()
+
+  return (
+    <Link
+      href={href}
+      className={`h-full flex items-center gap-3 tracking-[2.7px] ${
+        pathname === href
+          ? 'border-b-2 border-white'
+          : 'hover:border-b-2 hover:border-gray-500'
+      }`}
+    >
+      <span>{index}</span>
+      {content}
+    </Link>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,20 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '../assets/logo.svg'
+import { ActiveLink } from './ActiveLink'
 
 export function Header() {
   return (
     <header className="h-24 ml-14 flex items-center justify-between relative">
-      <Image src={logo} alt="Logo da Space Tourism" className="h-12 w-12" />
+      <Link href="/">
+        <Image src={logo} alt="Logo da Space Tourism" className="h-12 w-12" />
+      </Link>
       <div className="absolute h-px w-[473px] left-48 z-10 bg-white/20" />
       <nav className="flex items-center gap-12 h-full pl-[123px] pr-[165px] bg-gray-600/20 backdrop-blur-md">
-        <Link
-          href="/"
-          className="h-full flex items-center gap-3 tracking-[2.7px] border-b-2 border-white hover:border-b-2 hover:border-gray-500"
-        >
-          <span className="font-bold">00</span> HOME
-        </Link>
-
-        <Link
-          href="/destination"
-          className="h-full flex items-center gap-3 tracking-[2.7px] hover:border-b-2 hover:border-gray-500"
-        >
-          <span className="font-bold">01</span> DESTINATION
-        </Link>
-
-        <Link
-          href="/crew"
-          className="h-full flex items-center gap-3 tracking-[2.7px] hover:border-b-2 hover:border-gray-500"
-        >
-          <span className="font-bold">02</span> CREW
-        </Link>
-
-        <Link
-          href="/technology"
-          className="h-full flex items-center gap-3 tracking-[2.7px] hover:border-b-2 hover:border-gray-500"
-        >
-          <span className="font-bold">03</span> TECHNOLOGY
-        </Link>
+        <ActiveLink content="HOME" index="00" href="/" />
+        <ActiveLink content="DESTINATION" index="01" href="/destination" />
+        <ActiveLink content="CREW" index="02" href="/crew" />
+        <ActiveLink content="TECHNOLOGY" index="03" href="/technology" />
       </nav>
     </header>
   )
